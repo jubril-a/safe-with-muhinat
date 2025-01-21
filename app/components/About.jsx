d2f23b821f5e49324c25b97c475f224bd6f4e88a
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2025-01-21 12:32:37 +0100

    Completed About Section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,36 +1,37 @@
+import { MonaSans } from '../fontLoader';
 import styles from './About.module.scss';
 import Button from './Button';
 
 const About = () => {
     return (
-        <>
-            <div>
+        <div className={`${styles.about} constraint-500 section`}>
+            <div className={styles.aboutMain}>
                 <div className={styles.aboutImage}></div>
                 <div className={styles.aboutContent}>
-                    <h2>Hi 👋, I’m Muhinat</h2>
-                    <p className={styles.subheading}>I&apos;m a WHO-certified mental health and recovery coach dedicated to supporting young Muslimahs (ages 12–35) in navigating life’s challenges with faith, resilience, and clarity.</p>
-                    <p className={styles.subheading}>As a student of Islamic psychology and a mental health and recovery coach, my focus areas include self-discovery, self-esteem, identity development, confidence building, and recovery from porn addiction. I’m here to help you heal, grow, and step into the best version of yourself—mind, heart, and soul.  </p>
+                    <h2>Hi 👋, I&apos;m <span>Muhinat</span></h2>
+                    <p className={`${styles.introText} bodyText`}>I&apos;m a WHO-certified mental health and recovery coach dedicated to supporting young Muslimahs (ages 12–35) in navigating life’s challenges with faith, resilience, and clarity.</p>
+                    <p className={`${styles.introText} bodyText`}>As a student of Islamic psychology and a mental health and recovery coach, my focus areas include self-discovery, self-esteem, identity development, confidence building, and recovery from porn addiction. I’m here to help you heal, grow, and step into the best version of yourself—mind, heart, and soul.  </p>
                     <Button label="Book a Recovery Call" />
                 </div>
             </div>
-            <div className={styles.certs}>
-                <h3>Certifications</h3>
-                <div>
+            <div className={styles.certifications}>
+                <h3 className={`${MonaSans.className} ${styles.h3}`}>Certifications</h3>
+                <div className={styles.certs}>
                     <div className={styles.cert}>
-                        <div>
-                            <img src="null" alt="WHO logo" />
+                        <div className={`${styles.imgWrappper} center`}>
+                            <img src="/images/who.png" alt="" />
                         </div>
                         <p>WHO-Certified Mental Health & Recovery Coach</p>
                     </div>
                     <div className={styles.cert}>
-                        <div>
-                            <img src="null" alt="BSc. logo" />
+                        <div className={`${styles.imgWrappper} center`}>
+                            <img src="/images/bsc.png" alt="" />
                         </div>
                         <p>Bachelor’s in Islamic Psychology (in progress)</p>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./Button.module.scss"
+import { MonaSans } from "../fontLoader";
 
 const Button = ({ label }) => {
     return (
-        <button className={styles.button}>
+        <a href="#" className={`${MonaSans.className} ${styles.button}`}>
             <span>{label}</span>
             <Image src="/images/arrow.png" width="16" height="16" alt=""/>
-        </button>
+        </a>
     );
 };
 
